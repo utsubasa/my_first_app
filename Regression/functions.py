1a7a77f856f066d5846049640cae8b03512ea7b1
tsubasa umetani <tobachya@gmail.com>
2021-06-24 16:49:54 +0900

    My Django app, 7 commit

--- Regression/functions.py
+++ Regression/functions.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 import numpy as np
 import pandas as pd
 #import matplotlib.pyplot as plt
-#import sklearn
+import sklearn
 #from sklearn.model_selection import train_test_split
 #from sklearn.ensemble import RandomForestRegressor
 import pickle
