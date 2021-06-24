7ba146524956ee6c1e4b237edf328eebbcf264c1
tsubasa umetani <tobachya@gmail.com>
2021-06-24 15:41:32 +0900

    My Django app, second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
     'Regression.apps.RegressionConfig'
 ]
 
